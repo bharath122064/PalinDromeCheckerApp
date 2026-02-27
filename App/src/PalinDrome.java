ee8181108f2a35bc4952806d63e1eea9b6ba7c6e
bharath122064 <bm8376@srmist.edu.in>
2026-02-27 09:20:23 +0530

    uc5 submitted

--- App/src/PalinDrome.java
+++ App/src/PalinDrome.java
@@ -1,26 +1,18 @@
 public class PalinDrome {
     public static void main(String[] args) {
         String word = "radar";
-
-
-        char[] characters = word.toCharArray();
-
-
         int start = 0;
-        int end = characters.length - 1;
-
+        int end = word.length() - 1;
         boolean isPalindrome = true;
 
-
         while (start < end) {
-            if (characters[start] != characters[end]) {
+            if (word.charAt(start) != word.charAt(end)) {
                 isPalindrome = false;
                 break;
             }
             start++;
             end--;
         }
-
 
         System.out.println("Input: " + word);
         System.out.println("Is Palindrome?: " + isPalindrome);
