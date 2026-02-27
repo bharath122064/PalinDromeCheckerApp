05f060b45b9a6c2ab21e886d030a3b68d4262d19
bharath122064 <bm8376@srmist.edu.in>
2026-02-27 09:11:09 +0530

    uc4 submitted

--- App/src/PalinDrome.java
+++ App/src/PalinDrome.java
@@ -1,19 +1,29 @@
 public class PalinDrome {
     public static void main(String[] args) {
-        System.out.println("Welcome to polindrome app");
-        System.out.println("version 1.0");
-        System.out.println("System initialised");
-        String word="madam";
-        String reversed="";
-        for(int i=word.length()-1;i>=0;i--){
-            reversed = reversed + word.charAt(i);
+        String word = "radar";
+
+
+        char[] characters = word.toCharArray();
+
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        boolean isPalindrome = word.equals(reversed);
 
-        System.out.println("Input string: " + word);
-        System.out.println("Is it a palindrome: " + isPalindrome);
 
-        System.out.println("Program exited successfully.");
+        System.out.println("Input: " + word);
+        System.out.println("Is Palindrome?: " + isPalindrome);
     }
 
 }
